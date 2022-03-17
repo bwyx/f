d9833e733ece7cb548343af1a6fc65e2152f9451
Bayu Kurnia <hi@bayukurnia.com>
2022-03-17 15:37:00 +0700

    added prisma client plugin

---
+++ app/plugins/prisma.ts
@@ -0,0 +1,27 @@
+import fp from 'fastify-plugin'
+import Prisma from '@prisma/client'
+// PrismaClient not yet support esm
+// https://github.com/prisma/prisma/pull/4920
+const { PrismaClient } = Prisma
+
+import type { FastifyPluginAsync } from 'fastify'
+import type { PrismaClient as PrismaClientType } from '@prisma/client'
+
+// Use TypeScript module augmentation to declare the type of server.prisma to be PrismaClient
+declare module 'fastify' {
+  interface FastifyInstance {
+    prisma: PrismaClientType
+  }
+}
+
+const prismaPlugin: FastifyPluginAsync = fp(async (server, options) => {
+  const prisma = new PrismaClient()
+  await prisma.$connect()
+  // Make Prisma Client available through the fastify server instance: server.prisma
+  server.decorate('prisma', prisma)
+  server.addHook('onClose', async (server) => {
+    await server.prisma.$disconnect()
+  })
+})
+
+export default prismaPlugin
