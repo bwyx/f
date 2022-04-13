4950c34fa1aef130108c9842e852f35a8e2a10e9
Bayu Kurnia <hi@bayukurnia.com>
2022-04-13 19:35:16 +0700

    rework test

--- app/tests/services/user.service.spec.ts
+++ app/tests/services/user.service.spec.ts
@@ -22,59 +22,46 @@ describe('[Service: User]', () => {
     this.user.restore()
   })
 
-  describe('getUnique()', () => {
-    it('should call `findUnique()` only and once', async function () {
-      this.user.expects('findUnique').once()
-      this.user.expects('findFirst').never()
-      this.user.expects('findMany').never()
+  const user = {
+    name: 'John Doe',
+    email: 'john@doe.com',
+    password: 'strongpassword'
+  }
 
-      await userService.getUnique({ where: { id: 'userId' } })
+  describe('getUserByEmail()', () => {
+    it('should get a user by its email', async function () {
+      this.user.expects('findUnique').once().resolves()
+
+      await expect(userService.getUserByEmail(user.email)).to.be.fulfilled
+    })
+
+    it('should get a user by its email with options', async function () {
+      this.user.expects('findUnique').once().resolves()
+
+      await expect(
+        userService.getUserByEmail(user.email, {
+          select: {
+            id: false
+          }
+        })
+      ).to.be.fulfilled
     })
   })
 
-  describe('getFirst()', () => {
-    it('should call `findFirst()` only and once', async function () {
-      this.user.expects('findFirst').once()
-      this.user.expects('findUnique').never()
-      this.user.expects('findMany').never()
+  describe('createUser()', () => {
+    it('should create a new user', async function () {
+      this.user.expects('create').once().resolves()
 
-      await userService.getFirst({ where: { id: 'userId' } })
-    })
-  })
-
-  describe('query()', () => {
-    it('should call `findMany()` only and once', async function () {
-      this.user.expects('findMany').once()
-      this.user.expects('findUnique').never()
-      this.user.expects('findFirst').never()
-
-      await userService.query()
-    })
-  })
-
-  describe('create()', () => {
-    it('should call `create()` only and once', async function () {
-      const user = {
-        name: 'New User',
-        email: 'new@user.com',
-        password: 'strongpassword'
-      }
-
-      this.user.expects('create').once()
-      this.user.expects('findUnique').never()
-      this.user.expects('findFirst').never()
-      this.user.expects('findMany').never()
-
-      await userService.create(user)
+      await expect(
+        userService.createUser({
+          name: user.name,
+          email: user.email,
+          password: user.password
+        })
+      ).to.be.fulfilled
     })
 
-    it('should hash password', async function () {
-      const user = {
-        name: 'New User',
-        email: 'new@user.com',
-        password: 'strongpassword'
-      }
-
+    it('should hash user password', async function () {
       const args = sinon.match
         // has passwordHash field
         .hasNested('data.passwordHash')
@@ -85,43 +72,36 @@ describe('[Service: User]', () => {
 
       this.user.expects('create').withArgs(args)
 
-      await userService.create(user)
+      await expect(
+        userService.createUser({
+          name: user.name,
+          email: user.email,
+          password: user.password
+        })
+      ).to.be.fulfilled
     })
 
-    it('should throw [BadRequest] if record already exists', async function () {
-      const error = prismaError('P2002')
+    it('should throw [BadRequest] if the record already exists', async function () {
+      this.user.expects('create').once().throws(prismaError('P2002'))
 
-      const user = {
-        name: 'Already Exist',
-        email: 'already@exist.com',
-        password: 'strongpassword'
-      }
-
-      this.user.expects('create').once().throws(error)
-
-      await expect(userService.create(user)).to.be.rejectedWith(
+      await expect(userService.createUser(user)).to.be.rejectedWith(
         httpErrors.BadRequest
       )
     })
   })
 
-  describe('remove()', () => {
-    it('should call `delete()` only and once', async function () {
+  describe('deleteUserById()', () => {
+    it('should delete a user', async function () {
       this.user.expects('delete').once()
-      this.user.expects('findUnique').never()
-      this.user.expects('findFirst').never()
-      this.user.expects('findMany').never()
 
-      await userService.remove('userId')
+      await expect(userService.deleteUserById('userId')).to.be.fulfilled
     })
 
-    it('should not throws if no user record found', async function () {
-      const error = prismaError('P2025')
-
-      this.user.expects('delete').throws(error)
+    it('should not throw if no user record is found', async function () {
+      this.user.expects('delete').throws(prismaError('P2025'))
 
       await expect(
-        userService.remove('nonExistUserId')
+        userService.deleteUserById('nonExistUserId')
       ).to.be.fulfilled.and.eventually.equal(null)
     })
   })
