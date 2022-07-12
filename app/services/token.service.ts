fc261e060ed02f42a0f5f0b35d881109df4b19c6
Bayu Kurnia <hi@bayukurnia.com>
2022-07-12 23:40:23 +0700

    remove refreshToken-related codes

--- app/services/token.service.ts
+++ app/services/token.service.ts
@@ -1,4 +1,3 @@
-import crypto from 'crypto'
 import cryptoRandomString from 'crypto-random-string'
 import httpErrors from 'http-errors'
 
@@ -6,114 +5,11 @@ import type { FastifyJWT, JWT } from 'fastify-jwt'
 
 import { env, TokenTypes } from '../config/index.js'
 
-const key32 = (key: string) => key.substring(0, 32)
-
 export class TokenService {
-  private algorithm = 'aes-256-ctr'
-
-  constructor(private jwt: JWT, private key = env.APP_KEY) {}
+  constructor(private jwt: JWT) {}
 
   // eslint-disable-next-line class-methods-use-this
   generateNonce = () => cryptoRandomString({ length: 16, type: 'base64' })
-
-  encrypt = (text: string, iv = this.generateNonce()) => {
-    const cipher = crypto.createCipheriv(this.algorithm, key32(this.key), iv)
-
-    const encrypted = Buffer.concat([cipher.update(text), cipher.final()])
-
-    return { iv, content: encrypted.toString('base64') }
-  }
-
-  decrypt = (hash: { iv: string; content: string }) => {
-    const iv = Buffer.from(hash.iv)
-    const decipher = crypto.createDecipheriv(
-      this.algorithm,
-      key32(this.key),
-      iv
-    )
-
-    const decrypted = Buffer.concat([
-      decipher.update(Buffer.from(hash.content, 'base64')),
-      decipher.final()
-    ])
-
-    return decrypted.toString()
-  }
-
-  createSignature = (payload: string) => {
-    const hmac = crypto.createHmac('sha256', this.key)
-    hmac.update(payload)
-
-    return hmac.digest('base64')
-  }
-
-  verifySignature = (payload: string, signature: string) => {
-    const authenticSignature = this.createSignature(payload)
-    if (authenticSignature.length !== signature.length) {
-      return false
-    }
-
-    const isAuthentic = crypto.timingSafeEqual(
-      Buffer.from(authenticSignature),
-      Buffer.from(signature)
-    )
-
-    return isAuthentic
-  }
-
-  createOpaqueToken = (payload: string, nonce: string) => {
-    // convert payload to base64 to disguise . (dot) and other symbols
-    const base64Payload = Buffer.from(payload).toString('base64')
-
-    // sign payload
-    const signature = this.createSignature(base64Payload)
-    const signedPayload = `${base64Payload}.${signature}`
-
-    // encrypt signed payload
-    const { content, iv } = this.encrypt(signedPayload, nonce)
-    return `${content}.${iv}`
-  }
-
-  parseOpaqueToken = (token: string) => {
-    // decrypt
-    const [content, iv] = token.split('.')
-    const signedPayload = this.decrypt({ content, iv })
-
-    // verify signature
-    const [base64Payload, signature] = signedPayload.split('.')
-    if (!this.verifySignature(base64Payload, signature)) {
-      throw new httpErrors.Unauthorized('Invalid token signature')
-    }
-
-    // return parsed utf8 payload
-    return Buffer.from(base64Payload, 'base64').toString('utf-8')
-  }
-
-  generateRefreshToken = ({
-    sessionId,
-    nonce
-  }: {
-    sessionId: string
-    nonce: string
-  }) => {
-    const nextNonce = this.generateNonce()
-    const payload = `${sessionId}.${nextNonce}`
-
-    return this.createOpaqueToken(payload, nonce)
-  }
-
-  verifyRefreshToken = (refreshToken: string) => {
-    try {
-      const tokenNonce = refreshToken.split('.')[1]
-      const payload = this.parseOpaqueToken(refreshToken)
-
-      const [sessionId, nextNonce] = payload.split('.')
-
-      return { sessionId, nextNonce, tokenNonce }
-    } catch (e) {
-      throw new httpErrors.Unauthorized('Invalid refresh token')
-    }
-  }
 
   verifyJwt = (token: string, type: TokenTypes) => {
     let payload
--- app/tests/services/token.service.spec.ts
+++ app/tests/services/token.service.spec.ts
@@ -2,7 +2,6 @@
 import sinon from 'sinon'
 import chai, { expect } from 'chai'
 import chaiAsPromised from 'chai-as-promised'
-import httpErrors from 'http-errors'
 
 import { jwt } from '../mocks.js'
 import { TokenService } from '../../services/token.service.js'
@@ -10,9 +9,7 @@ import { TokenService } from '../../services/token.service.js'
 chai.use(chaiAsPromised)
 
 describe('[Service: Token]', () => {
-  const key = 'TEST_CcJWC4NvmVknXfcRvksqpsdtXwGP5SPj'
-
-  const tokenService = new TokenService(jwt, key)
+  const tokenService = new TokenService(jwt)
 
   beforeEach(function () {
     this.jwt = sinon.mock(jwt)
@@ -33,94 +30,6 @@ describe('[Service: Token]', () => {
       expect(
         tokenService.generateAccessToken({ userId: UUID, nonce: NONCE })
       ).to.be.a('string')
-    })
-  })
-
-  describe('encrypt-decrypt()', () => {
-    it('should be able to decrypt encrypted-payload', () => {
-      const text = 'hello you can see me'
-
-      const encrypted = tokenService.encrypt(text)
-      const decrypted = tokenService.decrypt(encrypted)
-
-      expect(encrypted.content).not.equal(text)
-      expect(decrypted).equal(text)
-    })
-  })
-
-  describe('create-verifySignature()', () => {
-    const data = 'authentic data requires signature'
-    const inauthenticData = 'this data is authentic. trust me'
-
-    it('should be able to verify signature', () => {
-      const signature = tokenService.createSignature(data)
-      const isAuthentic = tokenService.verifySignature(data, signature)
-
-      expect(isAuthentic).to.equal(true)
-    })
-
-    it('should not verify inauthentic data', () => {
-      const signature = tokenService.createSignature(data)
-      const isAuthentic = tokenService.verifySignature(
-        inauthenticData,
-        signature
-      )
-
-      expect(isAuthentic).to.equal(false)
-    })
-  })
-
-  describe('create-parseOpaqueToken()', () => {
-    it('should be able to process symbols', () => {
-      const symbols = `~!@#$%^&*()_+-={}[]:";<>,.?/'`
-
-      const opaqueToken = tokenService.createOpaqueToken(symbols, NONCE)
-      const parsedOpaqueToken = tokenService.parseOpaqueToken(opaqueToken)
-
-      expect(parsedOpaqueToken).to.be.equal(symbols)
-    })
-  })
-
-  describe('generate-verifyRefreshToken()', () => {
-    it('should generate a refreshToken', () => {
-      const refreshToken = tokenService.generateRefreshToken({
-        sessionId: UUID,
-        nonce: NONCE
-      })
-
-      expect(refreshToken).to.be.a('string')
-    })
-
-    it('should be able to verify and parse generated refresh token', () => {
-      const refreshToken = tokenService.generateRefreshToken({
-        sessionId: UUID,
-        nonce: NONCE
-      })
-      const valid = tokenService.verifyRefreshToken(refreshToken)
-
-      expect(valid.sessionId).equal(UUID)
-      expect(valid.tokenNonce).equal(NONCE)
-    })
-
-    it('should not verify modified refresh tokens', () => {
-      const refreshToken = tokenService.generateRefreshToken({
-        sessionId: UUID,
-        nonce: NONCE
-      })
-
-      const modifiedTokens = [
-        'randomguytryingtoguessthetoken',
-        `x${refreshToken}`,
-        `${refreshToken}x`,
-        refreshToken.slice(-5),
-        refreshToken.slice(0, -5)
-      ]
-
-      modifiedTokens.forEach((token) => {
-        const verify = () => tokenService.verifyRefreshToken(token)
-
-        expect(verify).to.throw(httpErrors.Unauthorized)
-      })
     })
   })
 })
